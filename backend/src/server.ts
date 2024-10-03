177eb6d93e8d4d5fc87a2ce1b3b4801b016d6224
chasejohnson626 <79180631+chasejohnson626@users.noreply.github.com>
2024-10-03 14:18:00 -0600

    wip: added users route

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,10 @@
 import { App } from '@/app';
 import { migrateToLatest } from '@database/database';
+import { UsersRoute } from '@features/users/users.route';
 
 async function startServer() {
   await migrateToLatest();
-  const app = new App([]);
+  const app = new App([new UsersRoute]);
   app.listen();
 }
 
