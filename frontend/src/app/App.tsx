77dcde326b91eeb4e00eb3a21865526d69f95392
SChan <schan5@ualberta.ca>
2024-10-09 17:16:12 -0600

    Testing css for calendar

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,5 +1,6 @@
 import Router from './router'
 import './App.css'
+import './pages/Components/BasicCalendar.css'
 import { BrowserRouter } from 'react-router-dom'
 import BasicCalendar from './pages/Components/BasicCalendar'
 
