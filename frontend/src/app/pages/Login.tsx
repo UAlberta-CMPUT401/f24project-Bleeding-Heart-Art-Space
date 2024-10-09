4f22853b98c9164c63401edbef5cd10d17b32442
chasejohnson626 <79180631+chasejohnson626@users.noreply.github.com>
2024-10-08 19:38:20 -0600

    wip: login page ui

--- frontend/src/app/pages/Login.tsx
+++ frontend/src/app/pages/Login.tsx
@@ -1,9 +1,35 @@
 import React from "react";
+import { Button, TextField } from "@mui/material";
+import styles from './Login.module.css';
 
 const Login: React.FC = () => {
   return (
     <>
-      <h1>Login Page</h1>
+      <h1>Login</h1>
+      <form
+        className={styles.loginForm}
+      >
+        <TextField
+          type="email"
+          className={styles.textField}
+          variant="outlined"
+          label="Email"
+        >
+        </TextField>
+        <TextField
+          type="password"
+          className={styles.textField}
+          variant="outlined"
+          label="Password"
+        >
+        </TextField>
+        <Button
+          type="submit"
+          variant="contained"
+        >
+          Login
+        </Button>
+      </form>
     </>
   );
 }
