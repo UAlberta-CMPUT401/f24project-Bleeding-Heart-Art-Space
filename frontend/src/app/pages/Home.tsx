0a0e7f79b85ec0c5fe6f92f78b769d94c5b6295c
chasejohnson626 <79180631+chasejohnson626@users.noreply.github.com>
2024-10-08 19:23:39 -0600

    feat: light/dark theme

--- frontend/src/app/pages/Home.tsx
+++ frontend/src/app/pages/Home.tsx
@@ -1,8 +1,21 @@
 import React from "react";
+import { Button } from "@mui/material";
 
 const Home: React.FC = () => {
   return (
-    <></>
+    <>
+      <h1>Bleeding Heart Art Space</h1>
+      <h2>Volunteer Management App</h2>
+      <Button 
+        variant="contained"
+        href="/login"
+        style={{
+          color: 'inherit',
+        }}
+      >
+        Login
+      </Button>
+    </>
   );
 }
 
--- frontend/src/app/router.tsx
+++ frontend/src/app/router.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
 
 const Router: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 }
