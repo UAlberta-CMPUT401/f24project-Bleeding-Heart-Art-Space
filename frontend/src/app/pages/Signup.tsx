4d25ff7d033d956bb411c7cc84ca089897e0e5e6
chasejohnson626 <79180631+chasejohnson626@users.noreply.github.com>
2024-10-08 22:17:41 -0600

    feat: signup page ui

--- frontend/src/app/pages/Signup.tsx
+++ frontend/src/app/pages/Signup.tsx
@@ -1,10 +1,63 @@
 import React from "react";
+import { Button, Card, Divider, TextField } from "@mui/material";
+import styles from './Login.module.css';
 import TopBar from "../../components/layout/topBar";
 
 const Signup: React.FC = () => {
   return (
     <TopBar>
       <h1>Sign-up</h1>
+      <Card
+        sx={{
+          marginX: 'auto',
+          maxWidth: '30rem',
+          padding: '2rem',
+        }}
+      >
+        <form
+          className={styles.loginForm}
+        >
+          <TextField
+            required
+            type="email"
+            variant="outlined"
+            label="Email"
+          >
+          </TextField>
+          <TextField
+            required
+            type="password"
+            variant="outlined"
+            label="Password"
+          >
+          </TextField>
+          <TextField
+            required
+            type="password"
+            variant="outlined"
+            label="Confirm Password"
+          >
+          </TextField>
+          <Button
+            type="submit"
+            variant="contained"
+          >
+            Sign-up
+          </Button>
+        </form>
+        <Divider
+          sx={{
+            marginY: '2rem',
+          }}
+        ></Divider>
+        <div>Already have an account?</div>
+        <Button
+          variant="outlined"
+          href="/login"
+        >
+          Login
+        </Button>
+      </Card>
     </TopBar>
   );
 }
