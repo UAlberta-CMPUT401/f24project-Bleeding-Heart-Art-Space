8f4341945d695f1bb5e30adcc2781b731abc53f6
SChan <schan5@ualberta.ca>
2024-10-09 16:30:23 -0600

    Basic Calendar feature

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,10 +1,15 @@
 import Router from './router'
 import './App.css'
 import { BrowserRouter } from 'react-router-dom'
+import BasicCalendar from './pages/Components/BasicCalendar'
+
 
 function App() {
   return (
     <BrowserRouter>
+      <div style={{height: "50vh"}}>
+        <BasicCalendar />
+      </div>
       <Router />
     </BrowserRouter>
   )
---
+++ frontend/src/app/pages/Components/BasicCalendar.tsx
@@ -0,0 +1,16 @@
+import moment from "moment";
+import Calendar from "../Calendar";
+
+
+
+const events = [
+    {
+        start: moment("2024-10-09T10:00:00").toDate(),
+        end: moment("2024-10-09T12:00:00").toDate(),
+        title: "Meeting that I missed lmao"
+    },
+];
+
+export default function BasicCalendar() {
+    return <Calendar events={events} />;
+}
