fdae935aa7c23bb5faa4b22089c61444e95b97d2
SChan <schan5@ualberta.ca>
2024-10-10 04:25:04 -0600

    dashboard basics

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,19 +1,30 @@
 import Router from './router'
-import './App.css'
-import './pages/Components/BasicCalendar.css'
-import { BrowserRouter } from 'react-router-dom'
-import BasicCalendar from './pages/Components/BasicCalendar'
-
+import './App.css';
+import './pages/Components/BasicCalendar.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import BasicCalendar from './pages/Components/BasicCalendar';
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
     <BrowserRouter>
-      <div style={{height: "50vh"}}>
-        <BasicCalendar />
+      <div className="app-container">
+        {/* Always show the Dashboard */}
+        <Dashboard />
+
+        {/* Render the page components based on the route */}
+        <div className="content-area">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            {/* <Route path="/create-event" element={<CreateEvent />} /> */}
+            <Route path="/calendar" element={<BasicCalendar />} />
+            {/* <Route path="/volunteer-management" element={<VolunteerManagement />} /> */}
+          </Routes>
+        </div>
       </div>
-      <Router />
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
