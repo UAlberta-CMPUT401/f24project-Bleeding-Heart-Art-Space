871561dd645d7869b6473cb27b55330b38e580d9
Samuel <schan5@ualberta.ca>
2024-10-12 13:30:12 -0600

    added volunteermangement page

---
+++ frontend/src/app/pages/VolunteerManagement.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import TopNav from './components/TopNav'; // Import the TopNav component
+import Dashboard from './Dashboard'; // Import the Dashboard component
+import './components/TopNav.css';
+import './Dashboard.css';
+
+const VolunteerManagement: React.FC = () => {
+    return (
+        <>
+            <div className="nav-container">
+                {/* Top Navigation Bar */}
+                <TopNav />
+
+                <div className="dashboard-container">
+                    {/* Sidebar Dashboard */}
+                    <Dashboard />
+
+                    {/* Main Content */}
+                    <div className="main-content">
+
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default VolunteerManagement;
