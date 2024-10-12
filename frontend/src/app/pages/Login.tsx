24cfab24e7d4c38ed764eff94ef9464f9213dc40
RJ-Gajjar <rishitgajjar93@gmail.com>
2024-10-12 09:55:54 -0600

    Setting up Firebase Login

--- frontend/src/app/pages/Login.tsx
+++ frontend/src/app/pages/Login.tsx
@@ -1,9 +1,36 @@
-import React from "react";
-import { Button, Card, Divider, TextField } from "@mui/material";
+import React, { useState } from "react";
+import { Button, Card, Divider, TextField, Alert } from "@mui/material";
 import styles from './Login.module.css';
 import TopBar from "../../components/layout/topBar";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../../firebase.ts";
+import { useNavigate } from "react-router-dom";
 
 const Login: React.FC = () => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState<string | null>(null);
+  
+  const navigate = useNavigate();
+
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setError(null);
+
+    try {
+      
+      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      console.log("User signed in:", userCredential.user);
+
+      
+      navigate("/Dashboard"); 
+    } catch (error: any) {
+      console.error("Error logging in:", error);
+      setError(error.message);
+    }
+  };
+
   return (
     <TopBar>
       <h1>Login</h1>
@@ -14,25 +41,32 @@ const Login: React.FC = () => {
           padding: '2rem',
         }}
       >
-        <form
-          className={styles.loginForm}
-        >
+        <form className={styles.loginForm} onSubmit={handleLogin}>
+          
+          {error && (
+            <Alert severity="error" style={{ marginBottom: "1rem" }}>
+              {error}
+            </Alert>
+          )}
+
           <TextField
             type="email"
             className={styles.textField}
             variant="outlined"
             label="Email"
             color="secondary"
-          >
-          </TextField>
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
           <TextField
             type="password"
             className={styles.textField}
             variant="outlined"
             label="Password"
             color="secondary"
-          >
-          </TextField>
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
           <Button
             type="submit"
             variant="contained"
@@ -40,18 +74,22 @@ const Login: React.FC = () => {
             Login
           </Button>
         </form>
+
         <Divider
           sx={{
             marginY: '2rem',
           }}
-        ></Divider>
+        />
+
         <div>Don't have an account yet?</div>
+
         <Button
           variant="outlined"
           href="/signup"
         >
           Sign-up
         </Button>
+
       </Card>
     </TopBar>
   );
