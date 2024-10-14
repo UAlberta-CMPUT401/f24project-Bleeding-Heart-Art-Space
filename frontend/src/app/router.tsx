c5b6e41f034865b5a9b15a00e7a6298647a50616
chasejohnson626 <79180631+chasejohnson626@users.noreply.github.com>
2024-10-14 11:06:28 -0600

    fix: dashboard route

--- frontend/src/app/router.tsx
+++ frontend/src/app/router.tsx
@@ -8,6 +8,7 @@ import Overview from './pages/Overview';
 import CreateEvent from "./pages/CreateEvent";
 import EditEvent from "./pages/EditEvent";
 import VolunteerManagement from "./pages/VolunteerManagement";
+import Dashboard from "./pages/Dashboard";
 
 interface Event {
   title: string;
@@ -38,6 +39,7 @@ const Router: React.FC = () => {
       <Route path="/overview" element={<Overview />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
+      <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/create-event" element={<CreateEvent isSidebarOpen={false} onAddEvent={handleAddEvent} />} />
       <Route path="/edit-event/:id" element={<EditEvent isSidebarOpen={false} />} />
       <Route path="/calendar" element={<BasicCalendar />} />
