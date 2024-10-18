2487ca4d1bff36631d5a171de7acf3e8e72b049d
chasejohnson626 <79180631+chasejohnson626@users.noreply.github.com>
2024-10-18 15:28:55 -0600

    feat: responsive mui side bar

--- frontend/src/app/pages/VolunteerManagement.tsx
+++ frontend/src/app/pages/VolunteerManagement.tsx
@@ -1,27 +1,12 @@
 import React from 'react';
-import TopNav from '@components/layout/TopNav'; // Import the TopNav component
-import Dashboard from '@components/layout/Dashboard'; // Import the Dashboard component
+import Dashboard from '@components/layout/Dashboard';
 import '@components/layout/TopNav.css';
 import '@components/layout/Dashboard.css';
 
 const VolunteerManagement: React.FC = () => {
     return (
-        <>
-            <div className="nav-container">
-                {/* Top Navigation Bar */}
-                <TopNav />
-
-                <div className="dashboard-container">
-                    {/* Sidebar Dashboard */}
-                    <Dashboard />
-
-                    {/* Main Content */}
-                    <div className="main-content">
-
-                    </div>
-                </div>
-            </div>
-        </>
+        <Dashboard>
+        </Dashboard>
     );
 };
 
