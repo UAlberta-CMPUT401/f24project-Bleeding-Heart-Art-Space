f967c3872baaa2c7a36a04d04c77387476dd2b20
Rushabh Shah <rnshah@ualberta.ca>
2024-10-22 14:54:01 -0600

    Modified the create and edit forms. Added a FAB for event creation to calendar

--- frontend/src/app/pages/BasicCalendar/BasicCalendar.tsx
+++ frontend/src/app/pages/BasicCalendar/BasicCalendar.tsx
@@ -1,6 +1,8 @@
 import EventCalendar from "./Components/CalendarEvent";
 import { useNavigate } from "react-router-dom"; // Import useNavigate
 import './BasicCalendar.css';
+import { Fab } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
 
 export default function BasicCalendar() {
 
@@ -16,9 +18,30 @@ export default function BasicCalendar() {
         navigate(`/create-event?start=${slotInfo.start.toISOString()}&end=${slotInfo.end.toISOString()}`); // Navigates to CreateEvent page with start, end dates and times
     };
 
+    // Function to handle FAB click
+    const handleFabClick = () => {
+        navigate('/create-event'); // Navigates to CreateEvent page without start and end dates
+    };
+
     return (
         <div style={{ maxWidth: 900 }}>
             <EventCalendar onEventClick={handleEventClick} onSlotSelect={handleSlotSelect} />
+
+            <Fab 
+                color="primary" 
+                aria-label="add" 
+                className="floating-button"
+                onClick={handleFabClick}
+                style={{
+                    position: 'fixed', 
+                    bottom: 16, 
+                    right: 16, 
+                    zIndex: 1000 
+                }}
+            >
+                <AddIcon />
+            </Fab>
+
         </div>
     );
 }
