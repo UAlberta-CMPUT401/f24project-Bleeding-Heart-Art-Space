ce29977d7c9ded7a1759845ab8833bdbfc3d19e3
chasejohnson626 <79180631+chasejohnson626@users.noreply.github.com>
2024-10-25 10:59:56 -0600

    refactor: clean up file

--- frontend/src/lib/context/AuthContext.tsx
+++ frontend/src/lib/context/AuthContext.tsx
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
-import { auth } from '../../utils/firebase'; // Import your Firebase config
+import { auth } from '../../utils/firebase';
 import { onAuthStateChanged, User } from 'firebase/auth';
 import { Outlet } from "react-router-dom";
 
@@ -9,34 +9,34 @@ interface AuthContextType {
     loading: boolean;
   }
   
-  // Initialize the context with a default value (can be null initially)
-  const AuthContext = createContext<AuthContextType | undefined>(undefined);
-  
-  export const AuthProvider: React.FC = () => {
-    const [user, setUser] = useState<User | null>(null); // Set the type to User | null
-    const [loading, setLoading] = useState(true);
-  
-    useEffect(() => {
-      const unsubscribe = onAuthStateChanged(auth, (user) => {
-        setUser(user); // This is fine now since we handle User | null
-        setLoading(false);
-      });
-  
-      return () => unsubscribe(); // Cleanup the subscription on unmount
-    }, []);
-  
-    return (
-      <AuthContext.Provider value={{ user, loading }}>
-        <Outlet/>
-      </AuthContext.Provider>
-    );
-  };
-  
-  // Custom hook to use the AuthContext
-  export const useAuth = () => {
-    const context = useContext(AuthContext);
-    if (!context) {
-      throw new Error('useAuth must be used within an AuthProvider');
-    }
-    return context;
-  };
+// Initialize the context with a default value (can be null initially)
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider: React.FC = () => {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, loading }}>
+      <Outlet/>
+    </AuthContext.Provider>
+  );
+};
+
+// Custom hook to use the AuthContext
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+  return context;
+};
