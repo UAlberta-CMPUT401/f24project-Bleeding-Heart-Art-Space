f8808ef0ef017a1edefc386ca51349d6df03abdc
Rushabh Shah <rnshah@ualberta.ca>
2024-10-25 19:26:36 -0600

    Introduced create events dialog instead of page. Refactored the calendar and create-event-dialog to use an event store for state management. #22

--- frontend/src/app/pages/BasicCalendar/Components/CalendarEvent.tsx
+++ frontend/src/app/pages/BasicCalendar/Components/CalendarEvent.tsx
@@ -1,49 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { Calendar, momentLocalizer, SlotInfo } from 'react-big-calendar';
 import moment from 'moment';
-import axios from 'axios';
-import 'react-big-calendar/lib/css/react-big-calendar.css'; // Import calendar styles
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import { useEventStore } from '@pages/EventStore/useEventStore';
 
-const localizer = momentLocalizer(moment); // Use moment for date localization
-
-const apiUrl = "http://localhost:3000/api"; // API URL
-
-interface Event {
-  id: string;
-  title: string;
-  start: Date;
-  end: Date;
-  venue: string;
-  address: string;
-}
+const localizer = momentLocalizer(moment); // Using moment for date localization
 
 interface EventCalendarProps {
-  events: { id: string; title: string; start: Date; end: Date }[];
   onEventClick: (eventId: string) => void;
   onSlotSelect: (slotInfo: SlotInfo) => void;
 }
 
 const EventCalendar: React.FC<EventCalendarProps> = ({ onEventClick, onSlotSelect  }) => {
-  const [events, setEvents] = useState<Event[]>([]);
+  const { events } = useEventStore(); //---> events function from from EventStore!
 
-  // Fetch events from the backend
   useEffect(() => {
-    axios.get(`${apiUrl}/events`)
-      .then(response => {
-        const fetchedEvents = response.data.map((event: any) => ({
-          id: event.id,
-          title: event.title,
-          start: new Date(event.start),
-          end: new Date(event.end),
-          venue: event.venue,
-          address: event.address
-        }));
-        setEvents(fetchedEvents); // Set events to the state
-      })
-      .catch(error => {
-        console.error("Error fetching events:", error);
-      });
-  }, []);
+  }, [events])
 
   return (
     <div style={{ height: 500 }}>
