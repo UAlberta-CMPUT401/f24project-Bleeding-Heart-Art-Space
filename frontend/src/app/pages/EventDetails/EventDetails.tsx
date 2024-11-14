38951cff9f16ff1ed40d5f397bf603f862f4914e
Rushabh Shah <rnshah@ualberta.ca>
2024-11-14 15:02:05 -0700

    refactor: remove check-in check-out from event details page

--- frontend/src/app/pages/EventDetails/EventDetails.tsx
+++ frontend/src/app/pages/EventDetails/EventDetails.tsx
@@ -1,14 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import { Grid, Typography, Button, Card, Container, Dialog, DialogTitle, DialogContent, DialogActions, Box, Snackbar, Alert } from '@mui/material';
+import { Grid, Typography, Button, Card, Container, Dialog, DialogTitle, DialogContent, DialogActions, Box } from '@mui/material';
 import EventIcon from '@mui/icons-material/Event';
 import PlaceIcon from '@mui/icons-material/Place';
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import AssignmentIndIcon from '@mui/icons-material/AssignmentInd';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import styles from './EventDetails.module.css';
-import { getVolunteerRoles, Shift, VolunteerRole, Event, isOk, getEventShifts, ShiftSignupUser, getEventShiftSignups, postShiftSignup, NewShiftSignup, checkin, checkout } from '@utils/fetch';
-import { isBefore, isAfter } from 'date-fns';
+import { getVolunteerRoles, Shift, VolunteerRole, Event, isOk, getEventShifts, ShiftSignupUser, getEventShiftSignups, postShiftSignup, NewShiftSignup } from '@utils/fetch';
 import { useAuth } from '@lib/context/AuthContext';
 import { useBackendUserStore } from '@stores/useBackendUserStore';
 import EditEventDialog from '@pages/EditEvent/EditEventDialog';
@@ -24,25 +23,14 @@ const EventDetails: React.FC = () => {
     const [userSignups, setUserSignups] = useState<ShiftSignupUser[]>([]);
     const [eventSignups, setEventSignups] = useState<ShiftSignupUser[]>([]);
     const [selectedShift, setSelectedShift] = useState<Shift | null>(null);
-
-    const [checkinDialogOpen, setCheckinDialogOpen] = useState(false);
-    const [checkinSignupId, setCheckinSignupId] = useState<number | null>(null);
-    const [checkoutDialogOpen, setCheckoutDialogOpen] = useState(false);
-    const [checkoutSignupId, setCheckoutSignupId] = useState<number | null>(null);
     const { user } = useAuth();
     const { backendUser } = useBackendUserStore();
     const [editDialogOpen, setEditDialogOpen] = useState(false);
-
-    //Snackbar alert states
+    const { events, fetchEvent } = useEventStore();
     const [signupFailSnackbarOpen, setSignupFailSnackbarOpen] = useState(false);
     const [signupFailSnackbarMessage, setSignupFailSnackbarMessage] = useState('');
     const [signupSuccessSnackbarOpen, setSignupSuccessSnackbarOpen] = useState(false);
     const [signupSuccessSnackbarMessage, setSignupSuccessSnackbarMessage] = useState('');
-    const [checkinSnackbarOpen, setCheckinSnackbarOpen] = useState(false);
-    const [checkinSnackbarMessage, setCheckinSnackbarMessage] = useState('');
-    const [checkoutSnackbarOpen, setCheckoutSnackbarOpen] = useState(false);
-    const [checkoutSnackbarMessage, setCheckoutSnackbarMessage] = useState('');
-    const { events, fetchEvent } = useEventStore();
 
     useEffect(() => {
         const eventId = Number(eventIdStr);
@@ -126,36 +114,6 @@ const EventDetails: React.FC = () => {
         })
     };
 
-    // Function to handle check-in
-    const handleCheckIn = (signupId: number) => {
-        if (!user) return;
-        const checkin_time = new Date().toISOString();
-
-        checkin(signupId, { checkin_time }, user).then(response => {
-            if (isOk(response.status)) {
-                alert('Checked in successfully!'); //DELETE THIS AFTER ADDING CHECK-IN
-                setCheckinSnackbarMessage('Checked in successfully!');
-                setCheckinSnackbarOpen(true);
-                setCheckinDialogOpen(false);
-            }
-        })
-    };
-
-    // Function to handle check-out
-    const handleCheckOut = (signupId: number) => {
-        if (!user) return;
-        const checkout_time = new Date().toISOString();
-
-        checkout(signupId, { checkout_time }, user).then(response => {
-            if (isOk(response.status)) {
-                alert('Checked out successfully!'); //DELETE THIS AFTER ADDING CHECK-IN
-                setCheckoutSnackbarMessage('Checked out successfully!');
-                setCheckoutSnackbarOpen(true);
-                setCheckoutDialogOpen(false);
-            }
-        })
-    };
-
     return (
         <>
             {event && <Container className={styles.container}>
@@ -200,10 +158,8 @@ const EventDetails: React.FC = () => {
                     </Typography>
                     <Grid container spacing={2}>
                         {shifts.map((shift, index) => {
-                            const currentTime = new Date(); // Get current time
                             const shiftStartTime = new Date(shift.start); // Shift start time
                             const shiftEndTime = new Date(shift.end); // Shift end time
-
                             return (
                                 <Grid item xs={12} sm={6} key={index}>
                                     <Card 
@@ -227,29 +183,6 @@ const EventDetails: React.FC = () => {
                                                         Signed up!
                                                     </Typography>
                                                 </Box>
-                                                {/* Check In / Check Out Buttons only show if current time is within shift's duration */}
-                                                {isAfter(currentTime, shiftStartTime) && isBefore(currentTime, shiftEndTime) && (
-                                                    <Grid container spacing={1} justifyContent="center" style={{ marginTop: '10px' }}>
-                                                        <Grid item>
-                                                            <Button 
-                                                                variant="contained" 
-                                                                color="primary" 
-                                                                onClick={() => {
-                                                                    setCheckinDialogOpen(true);
-                                                                    const signup = userSignups.find(s => s.shift_id === shift.id);
-                                                                    if (signup) setCheckinSignupId(signup.id);
-                                                                }}
-                                                            >
-                                                                Check In
-                                                            </Button>
-                                                        </Grid>
-                                                        <Grid item>
-                                                            <Button variant="contained" color="secondary" onClick={() => { setCheckoutSignupId(userSignups.find(s => s.shift_id === shift.id)?.id || null); setCheckoutDialogOpen(true); }}>
-                                                                Check Out
-                                                            </Button>
-                                                        </Grid>
-                                                    </Grid>
-                                                )}
                                             </>
                                         )}
                                     </Card>
@@ -269,42 +202,6 @@ const EventDetails: React.FC = () => {
                             <Button onClick={handleConfirmSignup} color="primary">Confirm</Button>
                         </DialogActions>
                     </Dialog>
-
-                    {/* Check-in Dialog */}
-                    <Dialog open={checkinDialogOpen} onClose={() => setCheckinDialogOpen(false)}>
-                        <DialogTitle>Check In</DialogTitle>
-                        <DialogContent>
-                            Are you sure you want to check in?
-                        </DialogContent>
-                        <DialogActions>
-                            <Button onClick={() => setCheckinDialogOpen(false)} color="secondary">Cancel</Button>
-                            <Button 
-                                onClick={() => {
-                                    if (checkinSignupId) handleCheckIn(checkinSignupId);
-                                }} 
-                                color="primary"
-                            >
-                                Check In
-                            </Button>
-                        </DialogActions>
-                    </Dialog>
-
-                    {/* Check Out Dialog */}
-                    <Dialog open={checkoutDialogOpen} onClose={() => setCheckoutDialogOpen(false)}>
-                        <DialogTitle>Check Out</DialogTitle>
-                        <DialogContent>
-                            <Typography>Are you sure you want to check out?</Typography>
-                        </DialogContent>
-                        <DialogActions>
-                            <Button onClick={() => setCheckoutDialogOpen(false)} color="primary">
-                                Cancel
-                            </Button>
-                            <Button onClick={() => { if (checkoutSignupId) handleCheckOut(checkoutSignupId); }}>
-                                Confirm Check Out
-                            </Button>
-                        </DialogActions>
-                    </Dialog>
-
 
                     {/* Edit Event Button */}
                     <Grid container spacing={2} justifyContent="center" style={{ marginTop: '20px' }}>
@@ -333,30 +230,6 @@ const EventDetails: React.FC = () => {
                 message={signupSuccessSnackbarMessage}
                 severity="success"
                 />
-
-                {/* Snackbar for Check-in Success. CHANGE THIS INTO SNACKBAR COMPONENT */}
-                <Snackbar
-                    open={checkinSnackbarOpen}
-                    autoHideDuration={3000}
-                    onClose={() => setCheckinSnackbarOpen(false)}
-                    anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
-                >
-                    <Alert onClose={() => setCheckinSnackbarOpen(false)} severity="success" variant="filled">
-                        {checkinSnackbarMessage}
-                    </Alert>
-                </Snackbar>
-
-                {/* Snackbar for Check-out Success. CHANGE THIS INTO SNACKBAR COMPONENT */}
-                <Snackbar
-                    open={checkoutSnackbarOpen}
-                    autoHideDuration={3000}
-                    onClose={() => setCheckoutSnackbarOpen(false)}
-                    anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
-                >
-                    <Alert onClose={() => setCheckoutSnackbarOpen(false)} severity="success" variant="filled">
-                        {checkoutSnackbarMessage}
-                    </Alert>
-                </Snackbar>
                 <EditEventDialog
                     open={editDialogOpen}
                     onClose={handleEditDialogClose}
