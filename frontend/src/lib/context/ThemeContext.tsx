daee92401116b2d9fb3ede03ad356f54ac30776a
amn-xyz <amaan2@ualberta.ca>
2024-12-01 01:39:42 -0700

    feat: Toggle theme implementation

--- frontend/src/lib/context/ThemeContext.tsx
+++ frontend/src/lib/context/ThemeContext.tsx
@@ -12,7 +12,12 @@ const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 export const ThemeProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const [isDarkMode, setIsDarkMode] = useState(() => {
     // Check if window is defined to avoid SSR issues
+    // Initialize theme from local storage, system preference if none
     if (typeof window !== 'undefined') {
+      const savedTheme = localStorage.getItem('theme')
+      if (savedTheme) {
+        return savedTheme === 'dark';
+      }
       return window.matchMedia('(prefers-color-scheme: dark)').matches;
     }
     return false;
@@ -22,11 +27,15 @@ export const ThemeProvider: React.FC<{ children: React.ReactNode }> = ({ childre
     const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
     
     const handleChange = (event: MediaQueryListEvent) => {
+      // Only update the state if the saved theme doesn't exist
+      if (!localStorage.getItem('theme')) {
+        setIsDarkMode(event.matches);
+      }
       setIsDarkMode(event.matches);
     };
 
     // Set the initial value
-    setIsDarkMode(mediaQuery.matches);
+    // setIsDarkMode(mediaQuery.matches);
 
     // Add the callback function as a listener for changes to the media query
     mediaQuery.addEventListener('change', handleChange);
@@ -34,6 +43,11 @@ export const ThemeProvider: React.FC<{ children: React.ReactNode }> = ({ childre
     // Remove the listener when the component is unmounted
     return () => mediaQuery.removeEventListener('change', handleChange);
   }, []);
+
+    // Save theme preference to local storage whenever it changes
+    useEffect(() => {
+      localStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
+    }, [isDarkMode]);
 
   const toggleTheme = useCallback(() => {
     setIsDarkMode(prevMode => !prevMode);
