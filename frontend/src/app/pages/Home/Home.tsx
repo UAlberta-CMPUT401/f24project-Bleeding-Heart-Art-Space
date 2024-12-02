429c73396c6f639fdce365cedb9221e510e65ab8
RJ-Gajjar <rishitgajjar93@gmail.com>
2024-12-02 15:17:43 -0700

    update: UI Changes in Home Page

--- frontend/src/app/pages/Home/Home.tsx
+++ frontend/src/app/pages/Home/Home.tsx
@@ -1,12 +1,48 @@
 import React from "react";
 import { Button } from "@mui/material";
 import { Link } from 'react-router-dom';
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // Import the carousel CSS
+import BHASLogo from '@assets/BHAS-Logo.png';
+import './Home.module.css';
+import Photo1 from '@assets/Photo1.jpeg';
+import Photo2 from '@assets/Photo2.jpg';
+import Photo3 from '@assets/Photo3.jpeg';
+import Photo4 from '@assets/Photo4.jpg';
 
 const Home: React.FC = () => {
   return (
-    <>
-      <h1>Bleeding Heart Art Space</h1>
-      <h2>Volunteer Management App</h2>
+    <div style={{ textAlign: 'center', padding: '20px' }}>
+
+      {/* Photo Carousel */}
+      <div style={{ marginTop: '20px' , marginBottom: '40px'}}>
+      <Carousel
+          showArrows={false}
+          autoPlay={true}
+          infiniteLoop={true}
+          showThumbs={false}
+          showStatus={false}
+          interval={3000}
+          transitionTime={1000}
+          emulateTouch={true}
+          centerMode={true}
+          centerSlidePercentage={70}
+        >
+          <div className="carousel-image-container">
+            <img src={Photo1} alt="Photo 1" className="carousel-image" />
+          </div>
+          <div className="carousel-image-container">
+            <img src={Photo2} alt="Photo 2" className="carousel-image" />
+          </div>
+          <div className="carousel-image-container">
+            <img src={Photo3} alt="Photo 3" className="carousel-image" />
+          </div>
+          <div className="carousel-image-container">
+            <img src={Photo4} alt="Photo 4" className="carousel-image" />
+          </div>
+        </Carousel>
+      </div>
+
       <Button 
         component={Link}
         variant="contained"
@@ -14,7 +50,7 @@ const Home: React.FC = () => {
       >
         Login
       </Button>
-    </>
+    </div>
   );
 }
 
